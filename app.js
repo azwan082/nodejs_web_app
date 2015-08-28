587e0ccc67b7be43d250c7797bdbd7a530be92ec
ResoAzwan <azwan082@gmail.com>
2015-08-28 16:07:52 +0800

    implement i18n

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
 var MongoStore = require('connect-mongo')(session);
+var i18n = require('i18n');
+var __ = i18n.__;
 var mongodb = require('./lib/mongodb');
 
 var index = require('./routes/index');
@@ -22,17 +24,41 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser('secret'));
 
 // public assets
 app.use(favicon(path.join(__dirname, 'public', 'dist', 'img', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'public', 'dist')));
 app.use(express.static(path.join(__dirname, 'bower_components')));
 
+// i18n
+var i18nArg = {
+  locales: ['en'],
+  defaultLocale: 'en',
+  cookie: 'lang',
+  directory: path.join(__dirname, 'i18n'),
+  indent: '  '
+};
+i18n.configure(i18nArg);
+app.use(i18n.init);
+app.use(function(req, res, next) {
+  var lang = req.query.lang;
+  if (lang) {
+    if (i18nArg.locales.indexOf(lang) == -1) {
+      lang = i18nArg.defaultLocale;
+    }
+    res.cookie(i18nArg.cookie, lang, {
+      maxAge: 900000,
+      httpOnly: true
+    });
+  }
+  next();
+});
+
 // session
 app.use(session({
   name: 'session_id',
-  secret: '53cR37',
+  secret: 'secret',
   saveUninitialized: false, // don't create session until something stored
   resave: false, // don't save session if unmodified
   store: new MongoStore({
@@ -41,7 +67,7 @@ app.use(session({
   })
 }));
 
-// session user
+// TODO session user
 app.use(function(req, res, next) {
   res.locals.user = {};
   next();
@@ -52,7 +78,7 @@ app.use('/', index);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  var err = new Error(__('Not Found'));
   err.status = 404;
   next(err);
 });
