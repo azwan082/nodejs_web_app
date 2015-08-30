ac24eac477ac4488bd8fda786c8aaa32b5d36989
Azwan b. Amit <azwan082@gmail.com>
2015-08-30 18:25:48 +0800

    fix email settings not saved

--- routes/index-settings.js
+++ routes/index-settings.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var __ = require('i18n').__;
+var iz = require('iz');
 var i18n = require('../lib/i18n');
 var data = require('moment-timezone/data/meta/latest.json');
+var User = require('../models/user');
 var router = express.Router();
 
 router.get('/', function(req, res) {
@@ -11,16 +13,15 @@ router.get('/', function(req, res) {
       selected: 'settings'
     },
     tab: req.query.tab || 'settings',
+    inputs: {},
     errors: {}
   };
   if (req.session._form) {
     arg.errors = req.session._form.errors || {};
+    arg.inputs = req.session._form.inputs || {};
     delete req.session._form;
   }
-  if (arg.tab == 'account') {
-    arg.title = __('Account');
-  }
-  else if (arg.tab == 'settings') {
+  if (arg.tab == 'settings') {
     var selectedLanguage = i18n.getSelectedLanguage(req);
     arg.languages = Object.keys(i18n.languages).map(function(id) {
       return {
@@ -50,40 +51,84 @@ router.get('/', function(req, res) {
     arg.timezones.sort(sort);
     arg.countries.sort(sort);
   }
+  else if (arg.tab == 'account') {
+    arg.title = __('Account');
+    arg.inputs.email = arg.inputs.email || req.user.email;
+  }
   res.render('index-settings', arg);
 });
 
 router.post('/', function(req, res) {
+  var tab = req.query.tab || '';
+  if (!tab) {
+    return res.redirect('/settings');
+  }
   var language = req.body.language || '';
   var country = req.body.country || '';
   var timezone = req.body.timezone || '';
+  var email = req.body.email || '';
+  var currentPassword = req.body.cpassword || '';
+  var newPassword = req.body.npassword || '';
   var errors = {};
-  if (Object.keys(i18n.languages).indexOf(language) == -1) {
-    errors.language = 'Invalid language';
+  if (tab == 'settings') {
+    if (Object.keys(i18n.languages).indexOf(language) == -1) {
+      errors.language = 'Invalid language';
+    }
+    if (Object.keys(data.zones).indexOf(timezone) == -1) {
+      errors.timezone = 'Invalid timezone';
+    }
+    if (Object.keys(data.countries).indexOf(country) == -1) {
+      errors.country = 'Invalid country';
+    }
   }
-  if (Object.keys(data.zones).indexOf(timezone) == -1) {
-    errors.timezone = 'Invalid timezone';
-  }
-  if (Object.keys(data.countries).indexOf(country) == -1) {
-    errors.country = 'Invalid country';
+  else if (tab == 'account') {
+    if (email.length === 0) {
+      errors.email = 'Email is required';
+    }
+    else if (!iz.email(email)) {
+      errors.email = 'Invalid email';
+    }
+    if (newPassword.length) {
+      if (req.user.isValidPassword(currentPassword)) {
+        errors.currentPassword = 'Wrong password';
+      } else {
+        if (newPassword.length < 5) {
+          errors.newPassword = 'New password too short';
+        }
+        else if (newPassword.length > 32) {
+          errors.newPassword = 'New password too long';
+        }
+      }
+    }
   }
   if (Object.keys(errors).length === 0) {
-    req.user.language = language;
-    req.user.timezone = timezone;
-    req.user.country = country;
-    req.user.save(function(err) {
-      if (err) {
-        req.flash('error', 'Failed to save changes');
-      } else {
-        req.flash('info', 'Settings saved');
+    if (tab == 'settings') {
+        req.user.language = language;
+        req.user.timezone = timezone;
+        req.user.country = country;
       }
-      res.redirect('/settings');
-    });
+      else if (tab == 'account') {
+        req.user.email = email;
+        if (newPassword) {
+          req.user.password = User.hashPassword(password);
+        }
+      }
+      req.user.save(function(err) {
+        if (err) {
+          req.flash('error', 'Failed to save changes');
+        } else {
+          req.flash('info', 'Settings saved');
+        }
+        res.redirect('/settings?tab=' + tab);
+      });
   } else {
     req.session._form = {
-      errors: errors
+      errors: errors,
+      inputs: {
+        email: email
+      }
     };
-    res.redirect('/settings');
+    res.redirect('/settings?tab=' + tab);
   }
 });
 
